eb7d8e92fab201f2aac591814d38d7f567caf818
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-15 15:02:01 -0300

    Add Project Routes :golf:

---
+++ src/Routes.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import OrphanagesMap from "./pages/OrphanagesMap";
+import Orphanage from "./pages/Orphanage";
+import CreateOrphanage from "./pages/CreateOrphanage";
+import userTheme from "./userTheme";
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/orphanages" exact component={OrphanagesMap} />
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default Routes;
