77c503001c795fc936932c7db0668ab72b32ea09
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-15 21:39:58 -0300

    Adding some configs to api :hammer:

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,6 @@
 import Axios from "axios";
-const ApiUrl = String(process.env.REACT_API_URL);
 const api = Axios.create({
-    baseURL: ApiUrl ? ApiUrl : "http://localhost:5500",
+    baseURL: "https://happy-servidor.herokuapp.com",
 });
 
 export default api;
