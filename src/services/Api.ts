428d1fd1e7791bb38cb47a728322fdbdae47eaaa
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-15 21:09:45 -0300

    :wrench: Api url
    Configuring api url

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
-
+const ApiUrl = String(process.env.REACT_API_URL);
 const api = Axios.create({
-    baseURL: 'http://localhost:5500',
+    baseURL: ApiUrl ? ApiUrl : "http://localhost:5500",
 });
 
 export default api;
