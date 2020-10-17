a96cb43b350445f72a4b51f6ecfe77140d4051d2
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-17 11:59:38 -0300

    Add new context to project (auth), now there's auth and toggle theme context :hammer:

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import Routes from "./Routes";
+import userAuth from "./userAuth";
 import ThemeContext from "./userTheme";
-import "./styles.css";
+
 import "leaflet/dist/leaflet.css";
+import "./styles.css";
 
 const App = () => {
-    const localStorageTheme = String(localStorage.getItem("theme"));
+    const localStorageTheme = localStorage.getItem("theme") || "light";
+    const localStorageLogin = Boolean(localStorage.getItem("logged"));
+
     const [theme, setTheme] = useState<string>(localStorageTheme);
+    const [isLogged, setIsLogged] = useState<boolean>(localStorageLogin);
+
     const toggleTheme = () => {
         const newTheme = theme == "light" ? "dark" : "light";
         setTheme(newTheme);
         localStorage.setItem("theme", newTheme);
     };
+
+    const setLogged = () => {
+        setIsLogged(true);
+        localStorage.setItem("logged", "true");
+    };
+
     return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            <Routes />
-        </ThemeContext.Provider>
+        <userAuth.Provider value={{ isLogged, setLogged }}>
+            <ThemeContext.Provider value={{ theme, toggleTheme }}>
+                <Routes />
+            </ThemeContext.Provider>
+        </userAuth.Provider>
     );
 };
 
