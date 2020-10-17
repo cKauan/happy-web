55b917e2255ecc121b26d21b14b9411c400625fa
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-17 12:46:27 -0300

    Update links and add a popup menu to choose different routes :hammer:

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { StyleHTMLAttributes } from "react";
 import logoSvg from "../../assets/logo.svg";
 
 import { FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { MdChildCare } from "react-icons/md";
 import "./styles.css";
 const Landing = () => {
-   
     return (
         <div id="page-landing">
             <div className="content-wrapper">
@@ -18,16 +20,43 @@ const Landing = () => {
                     <strong>Cascavel</strong>
                     <span>Ceará</span>
                 </div>
-                <Link
-                    to="/orphanages"
-                    className="enter-app"
-                    title="Visite orfanatos"
+                <Popup
+                    trigger={
+                        <div className="enter-app" title="Visite orfanatos">
+                            <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+                        </div>
+                    }
+                    position={"top right"}
+                    repositionOnResize
+                    contentStyle={{
+                        backgroundColor: "#0ec1cb",
+                        border: "2px solid #ffffff0f",
+                        boxShadow: "1px 1px 10px #0000003f",
+                        borderRadius: "20px",
+                        padding: "10px",
+                    }}
                 >
-                    <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-                </Link>
+                    <div className="popup-menu">
+                        <Link
+                            to="/orphanages"
+                            title="Visite orfanatos"
+                            className="popup-menu-link"
+                        >
+                            <p>Orfanatos</p>
+                            <MdChildCare size={20} color="#ffd666" />
+                        </Link>
+                        <Link
+                            to="/admin"
+                            title="Painel administrativo"
+                            className="popup-menu-link"
+                        >
+                            <p>Dashboard</p>
+                            <AiOutlineDashboard size={20} color="#86604f" />
+                        </Link>
+                    </div>
+                </Popup>
             </div>
         </div>
     );
 };
-
 export default Landing;
