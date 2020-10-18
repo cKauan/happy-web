eb8fb371b9d111cd078da7d1a9df0e384366732a
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-18 13:29:56 -0300

    Add a button to restrict access and some styles changes :art:

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React, { StyleHTMLAttributes } from "react";
 import logoSvg from "../../assets/logo.svg";
 
-import { FiArrowRight } from "react-icons/fi";
+import { FiArrowRight, FiMapPin } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import Popup from "reactjs-popup";
 import { AiOutlineDashboard } from "react-icons/ai";
@@ -17,44 +17,20 @@ const Landing = () => {
                     <p>Visite orfanatos e mude o dia de muitas crianças.</p>
                 </main>
                 <div className="location">
-                    <strong>Cascavel</strong>
+                    <FiMapPin size={22}/>
+                    <strong>Cascavel - </strong>
                     <span>Ceará</span>
                 </div>
-                <Popup
-                    trigger={
-                        <div className="enter-app" title="Visite orfanatos">
-                            <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-                        </div>
-                    }
-                    position={"top right"}
-                    repositionOnResize
-                    contentStyle={{
-                        backgroundColor: "#0ec1cb",
-                        border: "2px solid #ffffff0f",
-                        boxShadow: "1px 1px 10px #0000003f",
-                        borderRadius: "20px",
-                        padding: "10px",
-                    }}
+                <Link to="/admin" className="restrict-access">
+                    <h3>Acesso restrito</h3>
+                </Link>
+                <Link
+                    to="/orphanages"
+                    className="enter-app"
+                    title="Visite orfanatos"
                 >
-                    <div className="popup-menu">
-                        <Link
-                            to="/orphanages"
-                            title="Visite orfanatos"
-                            className="popup-menu-link"
-                        >
-                            <p>Orfanatos</p>
-                            <MdChildCare size={20} color="#ffd666" />
-                        </Link>
-                        <Link
-                            to="/admin"
-                            title="Painel administrativo"
-                            className="popup-menu-link"
-                        >
-                            <p>Dashboard</p>
-                            <AiOutlineDashboard size={20} color="#86604f" />
-                        </Link>
-                    </div>
-                </Popup>
+                    <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+                </Link>
             </div>
         </div>
     );
